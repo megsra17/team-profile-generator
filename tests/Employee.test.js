9b50e88053cb1515046e9051276ab0e9d6f006ad
Megan Rakow <meganrakow@Megans-MBP-2.lan>
2022-11-08 17:26:17 -0600

    added more test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,35 @@
-//import Employee from "../lib/Employee.js";
+import Employee from "../lib/Employee.js";
+
+describe("Employee Consructor", () => {
+  it("returns an employee name", () => {
+    //arrange
+    const name = "BoB";
+    const id = "35";
+    const email = "megsra@wi.rr.com";
+
+    //act
+    const employee = new Employee(name, id, email);
+    //assert
+    expect(employee.name).toBe(name);
+  });
+
+  it("Has correct id", () => {
+    //arrange
+    const name = "BoB";
+    const id = "35";
+    const email = "megsra@wi.rr.com";
+    //act
+    const employee = new Employee(name, id, email);
+    expect(employee.id).toBe(id);
+  });
+
+  it("Has the correct email", () => {
+    //arrange
+    const name = "BoB";
+    const id = "35";
+    const email = "megsra@wi.rr.com";
+    //act
+    const employee = new Employee(name, id, email);
+    expect(employee.email).toBe(email);
+  });
+});
